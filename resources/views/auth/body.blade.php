81f7271366af5254219924390bb7bd98d3d37fa6
Dede Kurniawan <dedekurniawan.dk67@gmail.com>
2021-09-05 19:14:47 +0700

    first commit

---
+++ resources/views/auth/body.blade.php
@@ -0,0 +1,13 @@
+<!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+<script src="assets/js/libs/jquery-3.1.1.min.js"></script>
+<script src="bootstrap/js/popper.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+<!-- END GLOBAL MANDATORY SCRIPTS -->
+<script src="assets/js/authentication/form-2.js"></script>
+<script>
+    var loaderElement = document.querySelector('#load_screen');
+        setTimeout( function() {
+            loaderElement.style.display = "none";
+        }, 3000);
+</script>
